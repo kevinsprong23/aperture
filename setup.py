c386e4e3b70ca933f45804e12f085c7d37ca73b1
Kevin Sprong <kevinsprong@gmail.com>
2014-09-30 16:58:23 -0400

    update homepage

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='aperture',
-      version='0.1.5',
+      version='0.1.6',
       description='Wrappers, settings, and extensions for Python/matplotlib',
-      url='https://github.com/kevinsprong/aperture',
+      url='https://github.com/kevinsprong23/aperture',
       author='Kevin Sprong',
       author_email='kevin@kevinsprong.com',
       license='MIT',
